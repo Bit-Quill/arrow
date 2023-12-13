dremio/flightsql-odbc

Implement Flight SQL result set
This implements ResultSet interface for Flight SQL.
Current implementation addresses:

- Access to Arrow’s String array and some basic conversions
- Releasing Arrow resources when closing
- BindColumn support for multiple rows
- GetData support + repeated calls to get more data

Change-Id: I9f0ba05ff2c0052d4a13a84d306007f674bb9a02

flight_sql/flight_sql_result_set_accessors.h
@@ -1,0 +1,31 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include "accessors/string_array_accessor.h"
+
+namespace driver {
+namespace flight_sql {
+
+class FlightSqlResultSet;
+
+std::unique_ptr<Accessor> CreateAccessor(const arrow::DataType &source_type,
+                                         odbcabstraction::DataType target_type);
+
+} // namespace flight_sql
+} // namespace driver

Bit-Quill/arrow

flightsql-odbc/flightsql-odbc/flightsql-odbc-clone/flightsql-odbc/flight_sql/flight_sql_result_set_accessors.h
@@ -1,0 +1,32 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include "accessors/main.h"
+
+namespace driver {
+namespace flight_sql {
+
+class FlightSqlResultSet;
+
+std::unique_ptr<Accessor>
+CreateAccessor(const arrow::DataType &source_type,
+               odbcabstraction::CDataType target_type);
+
+} // namespace flight_sql
+} // namespace driver