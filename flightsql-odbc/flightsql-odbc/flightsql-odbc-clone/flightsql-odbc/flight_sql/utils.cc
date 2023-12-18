dremio/flightsql-odbc

Enhance GetColumns with more metadata
This enhances GetColumns response with more accurate metadata, such as:
* COLUMN_SIZE / PRECISION
* CHAR_OCTET_LENGTH
* BUFFER_LENGTH
* DECIMAL_DIGITS / SCALE

Change-Id: Ibd39f634bc59acc7d75ab0f24c234d61d841aaf6

flight_sql/utils.cc
@@ -188,7 +188,8 @@ std::string GetTypeNameFromSqlDataType(int16_t data_type) {
                                                  std::to_string(data_type));
 }
 
-optional<int16_t> GetRadixFromSqlDataType(int16_t data_type) {
+optional<int16_t>
+GetRadixFromSqlDataType(odbcabstraction::SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_DECIMAL:
   case SqlDataType_NUMERIC:
@@ -206,7 +207,7 @@ optional<int16_t> GetRadixFromSqlDataType(int16_t data_type) {
   }
 }
 
-int16_t GetNonConciseDataType(int16_t data_type) {
+int16_t GetNonConciseDataType(odbcabstraction::SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_TYPE_DATE:
   case SqlDataType_TYPE_TIME:
@@ -231,7 +232,7 @@ int16_t GetNonConciseDataType(int16_t data_type) {
   }
 }
 
-optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type) {
+optional<int16_t> GetSqlDateTimeSubCode(SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_TYPE_DATE:
     return SqlDateTimeSubCode_DATE;
@@ -270,6 +271,88 @@ optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type) {
   }
 }
 
+optional<int16_t> GetCharOctetLength(SqlDataType data_type,
+                                     optional<int16_t> column_size) {
+  switch (data_type) {
+  case SqlDataType_CHAR:
+  case SqlDataType_VARCHAR:
+  case SqlDataType_LONGVARCHAR:
+    return column_size.has_value() ? column_size.value() : NO_TOTAL;
+  case SqlDataType_WCHAR:
+  case SqlDataType_WVARCHAR:
+  case SqlDataType_WLONGVARCHAR:
+    return column_size.has_value() ? (column_size.value() * sizeof(SqlWChar))
+                                   : NO_TOTAL;
+  case SqlDataType_BINARY:
+  case SqlDataType_VARBINARY:
+  case SqlDataType_LONGVARBINARY:
+    return column_size.has_value() ? column_size.value() : NO_TOTAL;
+  default:
+    return arrow::util::nullopt;
+  }
+}
+
+optional<int16_t> GetBufferLength(SqlDataType data_type,
+                                  optional<int16_t> column_size) {
+  switch (data_type) {
+  case SqlDataType_CHAR:
+  case SqlDataType_VARCHAR:
+  case SqlDataType_LONGVARCHAR:
+    return column_size;
+  case SqlDataType_WCHAR:
+  case SqlDataType_WVARCHAR:
+  case SqlDataType_WLONGVARCHAR:
+    return column_size.has_value() ? arrow::util::make_optional(column_size.value() * sizeof(SqlWChar))
+                                   : arrow::util::nullopt;
+  case SqlDataType_BINARY:
+  case SqlDataType_VARBINARY:
+  case SqlDataType_LONGVARBINARY:
+    return column_size;
+  case SqlDataType_DECIMAL:
+    return 19; // The same as sizeof(SQL_NUMERIC_STRUCT)
+  case SqlDataType_NUMERIC:
+    return 19; // The same as sizeof(SQL_NUMERIC_STRUCT)
+  case SqlDataType_BIT:
+  case SqlDataType_TINYINT:
+    return 1;
+  case SqlDataType_SMALLINT:
+    return 2;
+  case SqlDataType_INTEGER:
+    return 4;
+  case SqlDataType_BIGINT:
+    return 8;
+  case SqlDataType_REAL:
+    return 4;
+  case SqlDataType_FLOAT:
+  case SqlDataType_DOUBLE:
+    return 8;
+  case SqlDataType_TYPE_DATE:
+    return 6; // The same as sizeof(SQL_DATE_STRUCT)
+  case SqlDataType_TYPE_TIME:
+    return 6; // The same as sizeof(SQL_TIME_STRUCT)
+  case SqlDataType_TYPE_TIMESTAMP:
+    return 16; // The same as sizeof(SQL_TIME_STRUCT)
+  case SqlDataType_INTERVAL_MONTH:
+  case SqlDataType_INTERVAL_YEAR:
+  case SqlDataType_INTERVAL_YEAR_TO_MONTH:
+  case SqlDataType_INTERVAL_DAY:
+  case SqlDataType_INTERVAL_HOUR:
+  case SqlDataType_INTERVAL_MINUTE:
+  case SqlDataType_INTERVAL_SECOND:
+  case SqlDataType_INTERVAL_DAY_TO_HOUR:
+  case SqlDataType_INTERVAL_DAY_TO_MINUTE:
+  case SqlDataType_INTERVAL_DAY_TO_SECOND:
+  case SqlDataType_INTERVAL_HOUR_TO_MINUTE:
+  case SqlDataType_INTERVAL_HOUR_TO_SECOND:
+  case SqlDataType_INTERVAL_MINUTE_TO_SECOND:
+    return 28; // The same as sizeof(SQL_INTERVAL_STRUCT)
+  case SqlDataType_GUID:
+    return 16;
+  default:
+    return arrow::util::nullopt;
+  }
+}
+
 std::string ConvertSqlPatternToRegexString(const std::string &pattern) {
   static const std::string specials = "[]()|^-+*?{}$\\.";
 
@@ -283,21 +366,21 @@ std::string ConvertSqlPatternToRegexString(const std::string &pattern) {
     }
 
     switch (c) {
-      case '\\':
-        escape = true;
-        break;
-      case '_':
-        regex_str += '.';
-        break;
-      case '%':
-        regex_str += ".*";
-        break;
-      default:
-        if (specials.find(c) != std::string::npos) {
-          regex_str += '\\';
-        }
-        regex_str += c;
-        break;
+    case '\\':
+      escape = true;
+      break;
+    case '_':
+      regex_str += '.';
+      break;
+    case '%':
+      regex_str += ".*";
+      break;
+    default:
+      if (specials.find(c) != std::string::npos) {
+        regex_str += '\\';
+      }
+      regex_str += c;
+      break;
     }
   }
   return regex_str;

flight_sql/utils.h
@@ -20,6 +20,7 @@
 #include <arrow/flight/types.h>
 #include <arrow/util/optional.h>
 #include <boost/xpressive/xpressive.hpp>
+#include <codecvt>
 #include <odbcabstraction/exceptions.h>
 #include <odbcabstraction/types.h>
 
@@ -27,6 +28,16 @@ namespace driver {
 namespace flight_sql {
 
 using arrow::util::optional;
+
+#ifdef WITH_IODBC
+using SqlWChar = char32_t;
+using SqlWString = std::u32string;
+#else
+using SqlWChar = char16_t;
+using SqlWString = std::u16string;
+#endif
+using CharToWStrConverter =
+    std::wstring_convert<std::codecvt_utf8<SqlWChar>, SqlWChar>;
 
 inline void ThrowIfNotOK(const arrow::Status &status) {
   if (!status.ok()) {
@@ -56,11 +67,18 @@ GetDataTypeFromArrowField_V2(const std::shared_ptr<arrow::Field> &field);
 
 std::string GetTypeNameFromSqlDataType(int16_t data_type);
 
-optional<int16_t> GetRadixFromSqlDataType(int16_t data_type);
+optional<int16_t>
+GetRadixFromSqlDataType(odbcabstraction::SqlDataType data_type);
 
-int16_t GetNonConciseDataType(int16_t data_type);
+int16_t GetNonConciseDataType(odbcabstraction::SqlDataType data_type);
 
-optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type);
+optional<int16_t> GetSqlDateTimeSubCode(odbcabstraction::SqlDataType data_type);
+
+optional<int16_t> GetCharOctetLength(odbcabstraction::SqlDataType data_type,
+                                     optional<int16_t> column_size);
+
+optional<int16_t> GetBufferLength(odbcabstraction::SqlDataType data_type,
+                                  optional<int16_t> column_size);
 
 std::string ConvertSqlPatternToRegexString(const std::string &pattern);
 

Bit-Quill/arrow

flightsql-odbc/flightsql-odbc/flightsql-odbc-clone/flightsql-odbc/flight_sql/utils.cc
@@ -189,7 +189,8 @@ std::string GetTypeNameFromSqlDataType(int16_t data_type) {
                                                  std::to_string(data_type));
 }
 
-optional<int16_t> GetRadixFromSqlDataType(int16_t data_type) {
+optional<int16_t>
+GetRadixFromSqlDataType(odbcabstraction::SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_DECIMAL:
   case SqlDataType_NUMERIC:
@@ -207,7 +208,7 @@ optional<int16_t> GetRadixFromSqlDataType(int16_t data_type) {
   }
 }
 
-int16_t GetNonConciseDataType(int16_t data_type) {
+int16_t GetNonConciseDataType(odbcabstraction::SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_TYPE_DATE:
   case SqlDataType_TYPE_TIME:
@@ -232,7 +233,7 @@ int16_t GetNonConciseDataType(int16_t data_type) {
   }
 }
 
-optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type) {
+optional<int16_t> GetSqlDateTimeSubCode(SqlDataType data_type) {
   switch (data_type) {
   case SqlDataType_TYPE_DATE:
     return SqlDateTimeSubCode_DATE;
@@ -271,6 +272,88 @@ optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type) {
   }
 }
 
+optional<int32_t> GetCharOctetLength(SqlDataType data_type,
+                                     const optional<int32_t>& column_size) {
+  switch (data_type) {
+  case SqlDataType_CHAR:
+  case SqlDataType_VARCHAR:
+  case SqlDataType_LONGVARCHAR:
+    return column_size.has_value() ? column_size.value() : NO_TOTAL;
+  case SqlDataType_WCHAR:
+  case SqlDataType_WVARCHAR:
+  case SqlDataType_WLONGVARCHAR:
+    return column_size.has_value() ? (column_size.value() * sizeof(SqlWChar))
+                                   : NO_TOTAL;
+  case SqlDataType_BINARY:
+  case SqlDataType_VARBINARY:
+  case SqlDataType_LONGVARBINARY:
+    return column_size.has_value() ? column_size.value() : NO_TOTAL;
+  default:
+    return arrow::util::nullopt;
+  }
+}
+
+optional<int32_t> GetBufferLength(SqlDataType data_type,
+                                  const optional<int32_t>& column_size) {
+  switch (data_type) {
+  case SqlDataType_CHAR:
+  case SqlDataType_VARCHAR:
+  case SqlDataType_LONGVARCHAR:
+    return column_size;
+  case SqlDataType_WCHAR:
+  case SqlDataType_WVARCHAR:
+  case SqlDataType_WLONGVARCHAR:
+    return column_size.has_value() ? arrow::util::make_optional(column_size.value() * sizeof(SqlWChar))
+                                   : arrow::util::nullopt;
+  case SqlDataType_BINARY:
+  case SqlDataType_VARBINARY:
+  case SqlDataType_LONGVARBINARY:
+    return column_size;
+  case SqlDataType_DECIMAL:
+    return 19; // The same as sizeof(SQL_NUMERIC_STRUCT)
+  case SqlDataType_NUMERIC:
+    return 19; // The same as sizeof(SQL_NUMERIC_STRUCT)
+  case SqlDataType_BIT:
+  case SqlDataType_TINYINT:
+    return 1;
+  case SqlDataType_SMALLINT:
+    return 2;
+  case SqlDataType_INTEGER:
+    return 4;
+  case SqlDataType_BIGINT:
+    return 8;
+  case SqlDataType_REAL:
+    return 4;
+  case SqlDataType_FLOAT:
+  case SqlDataType_DOUBLE:
+    return 8;
+  case SqlDataType_TYPE_DATE:
+    return 6; // The same as sizeof(SQL_DATE_STRUCT)
+  case SqlDataType_TYPE_TIME:
+    return 6; // The same as sizeof(SQL_TIME_STRUCT)
+  case SqlDataType_TYPE_TIMESTAMP:
+    return 16; // The same as sizeof(SQL_TIME_STRUCT)
+  case SqlDataType_INTERVAL_MONTH:
+  case SqlDataType_INTERVAL_YEAR:
+  case SqlDataType_INTERVAL_YEAR_TO_MONTH:
+  case SqlDataType_INTERVAL_DAY:
+  case SqlDataType_INTERVAL_HOUR:
+  case SqlDataType_INTERVAL_MINUTE:
+  case SqlDataType_INTERVAL_SECOND:
+  case SqlDataType_INTERVAL_DAY_TO_HOUR:
+  case SqlDataType_INTERVAL_DAY_TO_MINUTE:
+  case SqlDataType_INTERVAL_DAY_TO_SECOND:
+  case SqlDataType_INTERVAL_HOUR_TO_MINUTE:
+  case SqlDataType_INTERVAL_HOUR_TO_SECOND:
+  case SqlDataType_INTERVAL_MINUTE_TO_SECOND:
+    return 28; // The same as sizeof(SQL_INTERVAL_STRUCT)
+  case SqlDataType_GUID:
+    return 16;
+  default:
+    return arrow::util::nullopt;
+  }
+}
+
 std::string ConvertSqlPatternToRegexString(const std::string &pattern) {
   static const std::string specials = "[]()|^-+*?{}$\\.";
 
@@ -284,21 +367,21 @@ std::string ConvertSqlPatternToRegexString(const std::string &pattern) {
     }
 
     switch (c) {
-      case '\\':
-        escape = true;
-        break;
-      case '_':
-        regex_str += '.';
-        break;
-      case '%':
-        regex_str += ".*";
-        break;
-      default:
-        if (specials.find(c) != std::string::npos) {
-          regex_str += '\\';
-        }
-        regex_str += c;
-        break;
+    case '\\':
+      escape = true;
+      break;
+    case '_':
+      regex_str += '.';
+      break;
+    case '%':
+      regex_str += ".*";
+      break;
+    default:
+      if (specials.find(c) != std::string::npos) {
+        regex_str += '\\';
+      }
+      regex_str += c;
+      break;
     }
   }
   return regex_str;

flightsql-odbc/flightsql-odbc/flightsql-odbc-clone/flightsql-odbc/flight_sql/utils.h
@@ -20,6 +20,7 @@
 #include <arrow/flight/types.h>
 #include <arrow/util/optional.h>
 #include <boost/xpressive/xpressive.hpp>
+#include <codecvt>
 #include <odbcabstraction/exceptions.h>
 #include <odbcabstraction/types.h>
 
@@ -27,6 +28,16 @@ namespace driver {
 namespace flight_sql {
 
 using arrow::util::optional;
+
+#ifdef WITH_IODBC
+using SqlWChar = char32_t;
+using SqlWString = std::u32string;
+#else
+using SqlWChar = char16_t;
+using SqlWString = std::u16string;
+#endif
+using CharToWStrConverter =
+    std::wstring_convert<std::codecvt_utf8<SqlWChar>, SqlWChar>;
 
 inline void ThrowIfNotOK(const arrow::Status &status) {
   if (!status.ok()) {
@@ -61,11 +72,18 @@ GetDataTypeFromArrowField_V2(const std::shared_ptr<arrow::Field> &field);
 
 std::string GetTypeNameFromSqlDataType(int16_t data_type);
 
-optional<int16_t> GetRadixFromSqlDataType(int16_t data_type);
+optional<int16_t>
+GetRadixFromSqlDataType(odbcabstraction::SqlDataType data_type);
 
-int16_t GetNonConciseDataType(int16_t data_type);
+int16_t GetNonConciseDataType(odbcabstraction::SqlDataType data_type);
 
-optional<int16_t> GetSqlDateTimeSubCode(int16_t data_type);
+optional<int16_t> GetSqlDateTimeSubCode(odbcabstraction::SqlDataType data_type);
+
+optional<int32_t> GetCharOctetLength(odbcabstraction::SqlDataType data_type,
+                                     const optional<int32_t>& column_size);
+
+optional<int32_t> GetBufferLength(odbcabstraction::SqlDataType data_type,
+                                  const optional<int32_t>& column_size);
 
 std::string ConvertSqlPatternToRegexString(const std::string &pattern);
 