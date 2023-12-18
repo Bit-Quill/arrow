dremio/flightsql-odbc

Implements accessor for date, time and timestamp
- Add an accessor to handle date32 and date64 arrays
- Add an accessor to handle timestamp arrays handling their time units
- Add an accessor to handle time array handling their time units
- Instantiate the accessor on the create method
- Add tests for the created accessors

Change-Id: Ib4dbb9a4c5ad7d4d8b261fd975992cf2cd4cd13c

flight_sql/accessors/calendar_utils.cc
@@ -15,12 +15,17 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#pragma once
+#include <cstdint>
+#include <ctime>
 
-#include "binary_array_accessor.h"
-#include "boolean_array_accessor.h"
-#include "date_array_accessor.h"
-#include "time_array_accessor.h"
-#include "timestamp_array_accessor.h"
-#include "primitive_array_accessor.h"
-#include "string_array_accessor.h"
+namespace driver {
+namespace flight_sql {
+void GetTimeForAccessor(tm& date, int64_t value) {
+  #if defined(_WIN32)
+    gmtime_s(&date, &value);
+  #else
+    gmtime_r(&value, &date);
+  #endif
+  }
+} // namespace flight_sql
+} // namespace driver

flight_sql/accessors/calendar_utils.h
@@ -15,12 +15,13 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <cstdint>
+#include <ctime>
+
 #pragma once
 
-#include "binary_array_accessor.h"
-#include "boolean_array_accessor.h"
-#include "date_array_accessor.h"
-#include "time_array_accessor.h"
-#include "timestamp_array_accessor.h"
-#include "primitive_array_accessor.h"
-#include "string_array_accessor.h"
+namespace driver {
+namespace flight_sql {
+  void GetTimeForAccessor(tm& date, int64_t value);
+} // namespace flight_sql
+} // namespace driver

Bit-Quill/arrow

flightsql-odbc/flightsql-odbc/flightsql-odbc-clone/flightsql-odbc/odbcabstraction/calendar_utils.cc
@@ -15,12 +15,17 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#pragma once
+#include <cstdint>
+#include <ctime>
 
-#include "binary_array_accessor.h"
-#include "boolean_array_accessor.h"
-#include "date_array_accessor.h"
-#include "time_array_accessor.h"
-#include "timestamp_array_accessor.h"
-#include "primitive_array_accessor.h"
-#include "string_array_accessor.h"
+namespace driver {
+namespace flight_sql {
+void GetTimeForMillisSinceEpoch(tm& date, int64_t value) {
+  #if defined(_WIN32)
+    gmtime_s(&date, &value);
+  #else
+    gmtime_r(&value, &date);
+  #endif
+  }
+} // namespace flight_sql
+} // namespace driver

flightsql-odbc/flightsql-odbc/flightsql-odbc-clone/flightsql-odbc/odbcabstraction/include/odbcabstraction/calendar_utils.h
@@ -15,12 +15,13 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <cstdint>
+#include <ctime>
+
 #pragma once
 
-#include "binary_array_accessor.h"
-#include "boolean_array_accessor.h"
-#include "date_array_accessor.h"
-#include "time_array_accessor.h"
-#include "timestamp_array_accessor.h"
-#include "primitive_array_accessor.h"
-#include "string_array_accessor.h"
+namespace driver {
+namespace flight_sql {
+  void GetTimeForMillisSinceEpoch(tm& date, int64_t value);
+} // namespace flight_sql
+} // namespace driver