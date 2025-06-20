6bfca8a8678be886ea274b5632db9652fd5b9778
Alina (Xi) Li <alina.li@improving.com>
2025-06-20 14:12:34 -0700

    Run Add DummyTestODBCMock only

--- cpp/src/arrow/flight/sql/odbc/tests/connection_attr_test.cc
+++ cpp/src/arrow/flight/sql/odbc/tests/connection_attr_test.cc
@@ -33,10 +33,15 @@ TEST(DummyTestSuite, DummyTestBool) {
   EXPECT_EQ(true_bool, true);
 }
 
-TEST_F(FlightSQLODBCRemoteTestBase, DummyTestODBCRemote) {
+TEST_F(FlightSQLODBCMockTestBase, DummyTestODBCMock) {
   bool true_bool = true;
   EXPECT_EQ(true_bool, true);
 }
+
+//TEST_F(FlightSQLODBCRemoteTestBase, DummyTestODBCRemote) {
+//  bool true_bool = true;
+//  EXPECT_EQ(true_bool, true);
+//}
 
 // FlightSQLODBCTestBase is known to cause seg fault
 //TYPED_TEST(FlightSQLODBCTestBase, DummyTestODBC) {
