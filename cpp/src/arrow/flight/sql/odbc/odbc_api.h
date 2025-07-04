4546dbf7615468e9033e9fc98c4494baaf45cac9
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2025-07-04 14:08:22 -0700

    SQLFetch & SQLGetData Implementation
    SQLGetStmtAttr stub implementation
    
    Stub call for SQLGetStmtAttr
    
    Enable statement handle allocation
    
    Implement SQLExecDirect - test hang issue
    
    Update odbc_api.cc
    
    Run `ShutdownProtobufLibrary` after all ODBC tests
    
    Resolves test hanging problem
    
    Fix missing break statement in SQLGetDiagRec
    
    Add tests
    
    Lint fixes
    
    Run ShutdownProtobufLibrary as part of test environment
    
    Add debug messages
    
    Draft code
    
    Remove drafts
    
    Add comment
    
    Add comment for GH-46889
    
    Update statement test headers
    
    Pass call options to executed prepared statement
    
    `call_options_` contains the authentication token which is needed
    
    SQLFetch & SQLGetData initial impl
    
    EVERYTHING before this commit is for SQLExecDirect
    
    SQLFetch and SQLGetData TestSQLExecDirectSimpleQuery test
    
    Make GetData() return SQLRETURN
    
    `TestSQLExecDirectDataQuery` for remote and mock servers
    
    Remove unneeded test case
    
    Add more data types
    
    * Add SQL_GUID in getCTypeForSQLType
    
    * Add data types in query
    
    * Add checks for columns 1 to 25
    
    * Update statement_test.cc
    
    Add varbinary test
    
    * Implement SQLNumResultCols
    
    Will leave tests for later
    
    * Implement SQLRowCount
    
    * Implement SQLMoreResults
    
    * Work on date retrieval fix
    
    The `TestSQLExecDirectDataQuery` checks for date is not working.
    
    * Fix date retrieval error by importing fix from Paul
    
    * imported fix from https://github.com/dremio/flightsql-odbc/commit/d44d862bd07f05328f5fcfd68a8f40357aa072fa, an Apache-Licensed repository
    
    * Add more checks for get data
    
    * Add more tests
    
    * Continue work on float truncation test
    
    * Disable float truncation test
    
    * Address comments from James
    
    - do not return errors for invalid rowCountPtr and columnCountPtr
    - add static cast for columnCount
    - Add tests with SQL_C_DEFAULT as target type
    - Add test checks for indicator
    - Fix SQL_NUMERIC, SQL_BIT, SQL_FLOAT target type
    
    * Fix build issues in CI
    
    * More fix for CI
    
    * Add test for invalid buffer length

--- cpp/src/arrow/flight/sql/odbc/odbc_api.h
+++ cpp/src/arrow/flight/sql/odbc/odbc_api.h
@@ -63,4 +63,10 @@ SQLRETURN SQLGetInfo(SQLHDBC conn, SQLUSMALLINT infoType, SQLPOINTER infoValuePt
 SQLRETURN SQLGetStmtAttr(SQLHSTMT stmt, SQLINTEGER attribute, SQLPOINTER valuePtr,
                          SQLINTEGER bufferLength, SQLINTEGER* stringLengthPtr);
 SQLRETURN SQLExecDirect(SQLHSTMT stmt, SQLWCHAR* queryText, SQLINTEGER textLength);
+SQLRETURN SQLFetch(SQLHSTMT stmt);
+SQLRETURN SQLGetData(SQLHSTMT stmt, SQLUSMALLINT recordNumber, SQLSMALLINT cType,
+                     SQLPOINTER dataPtr, SQLLEN bufferLength, SQLLEN* indicatorPtr);
+SQLRETURN SQLMoreResults(SQLHSTMT stmt);
+SQLRETURN SQLNumResultCols(SQLHSTMT stmt, SQLSMALLINT* columnCountPtr);
+SQLRETURN SQLRowCount(SQLHSTMT stmt, SQLLEN* rowCountPtr);
 }  // namespace arrow
