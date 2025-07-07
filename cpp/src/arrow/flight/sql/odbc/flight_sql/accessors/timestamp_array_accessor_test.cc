1788b84dde744c8e5cbdd4936e5920d05a5eaa87
Alina (Xi) Li <alina.li@improving.com>
2025-07-07 13:42:13 -0700

    Test time changes in arrow#47017
    Testing changes at https://github.com/apache/arrow/pull/47017

--- cpp/src/arrow/flight/sql/odbc/flight_sql/accessors/timestamp_array_accessor_test.cc
+++ cpp/src/arrow/flight/sql/odbc/flight_sql/accessors/timestamp_array_accessor_test.cc
@@ -35,8 +35,10 @@ using odbcabstraction::GetTimeForSecondsSinceEpoch;
 
 TEST(TEST_TIMESTAMP, TIMESTAMP_WITH_MILLI) {
   std::vector<int64_t> values = {
-      86400370,  172800000, 259200000, 1649793238110LL, 345600000, 432000000, 518400000,
-      -86399000, 0,         -86399999, -86399001,       86400001,  86400999};
+      86400370,  172800000,      259200000,      1649793238110LL,
+      345600000, 432000000,      518400000,      -86399000,
+      0,         -86399999,      -86399001,      86400001,
+      86400999,  -3786912000000, -5364662400000, -1500};
   std::vector<TIMESTAMP_STRUCT> expected = {
       /* year(16), month(u16), day(u16), hour(u16), minute(u16), second(u16),
          fraction(u32) */
@@ -55,7 +57,9 @@ TEST(TEST_TIMESTAMP, TIMESTAMP_WITH_MILLI) {
       {1969, 12, 31, 0, 0, 0, 999000000},
       {1970, 1, 2, 0, 0, 0, 1000000},
       {1970, 1, 2, 0, 0, 0, 999000000},
-  };
+      {1849, 12, 31, 0, 0, 0, 0U},
+      {1800, 1, 1, 0, 0, 0, 0U},
+      {1969, 12, 31, 23, 59, 58, 500000000U}};
 
   std::shared_ptr<Array> timestamp_array;
 
