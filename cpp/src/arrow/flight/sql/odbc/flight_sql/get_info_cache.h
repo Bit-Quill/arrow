924d3ab5c2ec06ce6b5c05ffbb8f9c6c3547d47d
Alina (Xi) Li <96995091+alinaliBQ@users.noreply.github.com>
2025-07-17 12:58:52 -0700

    GH-46584 Iterate endpoint
    * use suggestion from James for one-liner change to return `Status::OK` directly
    
    * fix for iterating endpoints, it is based on JDBC's fix for the same bug
    
    * save value of `FlightClientOptions`
    
    * Use `TestFlightServer`
    
    Add `arrow_flight_sql_shared` to enable usages for `TestFlightServer`
    
    * Fix build errors from missing `gmock`
    
    * Add checks for array data
    
    Update flight_sql_stream_chunk_buffer_test.cc
    
    Add `FlightStreamChunkBufferTest` unit test
    
    Draft test with `FlightSQLODBCMockEndpointTestBase`
    
    * Reference GH-47117 and Clean up code
    
    * Add driver and DSN to built-in properties to not pass driver/dsn as attributes to server
    
    * Allow `=` in values inside connection strings, fixes connectivity problems
    
    * Address comments from Rob

--- cpp/src/arrow/flight/sql/odbc/flight_sql/get_info_cache.h
+++ cpp/src/arrow/flight/sql/odbc/flight_sql/get_info_cache.h
@@ -30,13 +30,15 @@ namespace flight_sql {
 class GetInfoCache {
  private:
   std::unordered_map<uint16_t, driver::odbcabstraction::Connection::Info> info_;
+  arrow::flight::FlightClientOptions& client_options_;
   arrow::flight::FlightCallOptions& call_options_;
   std::unique_ptr<arrow::flight::sql::FlightSqlClient>& sql_client_;
   std::mutex mutex_;
   std::atomic<bool> has_server_info_;
 
  public:
-  GetInfoCache(arrow::flight::FlightCallOptions& call_options,
+  GetInfoCache(arrow::flight::FlightClientOptions& client_options,
+               arrow::flight::FlightCallOptions& call_options,
                std::unique_ptr<arrow::flight::sql::FlightSqlClient>& client,
                const std::string& driver_version);
   void SetProperty(uint16_t property, driver::odbcabstraction::Connection::Info value);
