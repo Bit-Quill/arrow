8bb97463d4c63f9c7cae27c0f08844a7013d42a0
Alina (Xi) Li <alina.li@improving.com>
2025-08-05 14:22:09 -0700

    in-progress SQLError test
    * Update SQLGetDiagRec and SQLGetDiagField tests also

--- cpp/src/arrow/flight/sql/odbc/tests/errors_test.cc
+++ cpp/src/arrow/flight/sql/odbc/tests/errors_test.cc
@@ -120,12 +120,7 @@ TYPED_TEST(FlightSQLODBCTestBase, TestSQLGetDiagFieldWForConnectFailure) {
 
   EXPECT_EQ(ret, SQL_SUCCESS);
 
-  // 28000
-  EXPECT_EQ(sql_state[0], '2');
-  EXPECT_EQ(sql_state[1], '8');
-  EXPECT_EQ(sql_state[2], '0');
-  EXPECT_EQ(sql_state[3], '0');
-  EXPECT_EQ(sql_state[4], '0');
+  EXPECT_EQ(std::wstring(sql_state), std::wstring(L"28000"));
 
   // Free connection handle
   ret = SQLFreeHandle(SQL_HANDLE_DBC, conn);
@@ -254,12 +249,9 @@ TYPED_TEST(FlightSQLODBCTestBase, TestSQLGetDiagRecForConnectFailure) {
 
   EXPECT_EQ(native_error, 200);
 
-  // 28000
-  EXPECT_EQ(sql_state[0], '2');
-  EXPECT_EQ(sql_state[1], '8');
-  EXPECT_EQ(sql_state[2], '0');
-  EXPECT_EQ(sql_state[3], '0');
-  EXPECT_EQ(sql_state[4], '0');
+  EXPECT_EQ(std::wstring(sql_state), std::wstring(L"28000"));
+
+  EXPECT_TRUE(!std::wstring(message).empty());
 
   // Free connection handle
   ret = SQLFreeHandle(SQL_HANDLE_DBC, conn);
@@ -270,6 +262,71 @@ TYPED_TEST(FlightSQLODBCTestBase, TestSQLGetDiagRecForConnectFailure) {
   ret = SQLFreeHandle(SQL_HANDLE_ENV, env);
 
   EXPECT_EQ(ret, SQL_SUCCESS);
+}
+
+// -AL- todo check for returns. Separate input tests from actual tests with values.
+TYPED_TEST(FlightSQLODBCTestBase, TestSQLErrorInputData) {
+  // Test ODBC 2.0 API SQLError. Driver manager maps SQLError to SQLGetDiagRec.
+  // SQLError does not post diagnostic records for itself.
+  this->connect();
+
+  SQLRETURN ret = SQLError(this->env, 0, 0, 0, 0, 0, 0, 0);
+
+  EXPECT_EQ(ret, SQL_NO_DATA);
+
+  ret = SQLError(0, this->conn, 0, 0, 0, 0, 0, 0);
+
+  EXPECT_EQ(ret, SQL_NO_DATA);
+
+  ret = SQLError(0, 0, this->stmt, 0, 0, 0, 0, 0);
+
+  EXPECT_EQ(ret, SQL_NO_DATA);
+
+  ret = SQLError(0, 0, 0, 0, 0, 0, 0, 0);
+
+  EXPECT_EQ(ret, SQL_INVALID_HANDLE);
+
+  this->disconnect();
+}
+
+// -AL- todo:
+// add an [ ] 1) env error -> wrong env attribute? - see TestSQLSetEnvAttrODBCVersionInvalid
+// [ ] 2) conn error -> set unsettable conn attr SQLSetConnectAttr? - see TestSQLSetConnectAttrAsyncDbcEventUnsupported
+// [x] 3) stmt error.
+
+TYPED_TEST(FlightSQLODBCTestBase, TestSQLErrorStmtError) {
+  // Test ODBC 2.0 API SQLError.
+  // Known Windows Driver Manager (DM) behavior:
+  // When application passes buffer length greater than SQL_MAX_MESSAGE_LENGTH (512),
+  // DM passes 512 as buffer length to SQLError.
+  this->connect();
+
+  std::wstring wsql = L"1";
+  std::vector<SQLWCHAR> sql0(wsql.begin(), wsql.end());
+
+  SQLRETURN ret =
+      SQLExecDirect(this->stmt, &sql0[0], static_cast<SQLINTEGER>(sql0.size()));
+
+  EXPECT_EQ(ret, SQL_ERROR);
+
+  SQLWCHAR sql_state[6] = {0};
+  SQLINTEGER native_error = 0;
+  SQLWCHAR message[SQL_MAX_MESSAGE_LENGTH] = {0};
+  SQLSMALLINT message_length = 0;
+  ret = SQLError(0, 0, this->stmt, sql_state, &native_error, message,
+                 SQL_MAX_MESSAGE_LENGTH, &message_length);
+
+  EXPECT_EQ(ret, SQL_SUCCESS);
+
+  EXPECT_GT(message_length, 70);
+
+  EXPECT_EQ(native_error, 100);
+
+  EXPECT_EQ(std::wstring(sql_state), std::wstring(L"HY000"));
+
+  EXPECT_TRUE(!std::wstring(message).empty());
+
+  this->disconnect();
 }
 
 }  // namespace arrow::flight::sql::odbc
