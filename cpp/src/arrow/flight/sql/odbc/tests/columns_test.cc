475b2c302dd2fb012cde82b91faf6a52ed3167bb
Alina (Xi) Li <alina.li@improving.com>
2025-09-05 14:51:41 -0700

    Fix build warnings treated as error
    SQLDescribeCol expects `bufCharLen` to be of type `SQLSMALLINT`, so build warnings occur when `bufCharLen` is set to `SQLINTEGER`.

--- cpp/src/arrow/flight/sql/odbc/tests/columns_test.cc
+++ cpp/src/arrow/flight/sql/odbc/tests/columns_test.cc
@@ -2333,7 +2333,8 @@ TEST_F(FlightSQLODBCMockTestBase, SQLDescribeColValidateInput) {
   SQLUSMALLINT outOfRangeColumn = 4;
   SQLUSMALLINT negativeColumn = -1;
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT dataType = 0;
   SQLULEN columnSize = 0;
@@ -2378,7 +2379,8 @@ TEST_F(FlightSQLODBCMockTestBase, SQLDescribeColQueryAllDataTypesMetadata) {
   this->connect();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2454,7 +2456,8 @@ TEST_F(FlightSQLODBCRemoteTestBase, SQLDescribeColQueryAllDataTypesMetadata) {
   this->connect();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2537,7 +2540,8 @@ TEST_F(FlightSQLODBCRemoteTestBase, SQLDescribeColODBCTestTableMetadata) {
   this->connect();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2599,7 +2603,8 @@ TEST_F(FlightSQLODBCRemoteTestBase, SQLDescribeColODBCTestTableMetadataODBC2) {
   this->connect(SQL_OV_ODBC2);
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2661,7 +2666,8 @@ TEST_F(FlightSQLODBCMockTestBase, SQLDescribeColAllTypesTableMetadata) {
   this->CreateTableAllDataType();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2717,7 +2723,8 @@ TEST_F(FlightSQLODBCMockTestBase, SQLDescribeColUnicodeTableMetadata) {
   this->CreateUnicodeTable();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2761,7 +2768,8 @@ TYPED_TEST(FlightSQLODBCTestBase, SQLColumnsGetMetadataBySQLDescribeCol) {
   this->connect();
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
@@ -2821,7 +2829,8 @@ TYPED_TEST(FlightSQLODBCTestBase, SQLColumnsGetMetadataBySQLDescribeColODBC2) {
   this->connect(SQL_OV_ODBC2);
 
   SQLWCHAR columnName[1024];
-  SQLINTEGER bufCharLen = sizeof(columnName) / ODBC::GetSqlWCharSize();
+  SQLSMALLINT bufCharLen =
+      static_cast<SQLSMALLINT>(sizeof(columnName) / ODBC::GetSqlWCharSize());
   SQLSMALLINT nameLength = 0;
   SQLSMALLINT columnDataType = 0;
   SQLULEN columnSize = 0;
