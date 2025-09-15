aef676c940f31c849314d997f2d383bc6dce5a13
Alina (Xi) Li <alina.li@improving.com>
2025-09-15 15:37:28 -0700

    Fix build issues
    TODO: fix spdlog issue

--- cpp/src/arrow/flight/sql/odbc/entry_points.cc
+++ cpp/src/arrow/flight/sql/odbc/entry_points.cc
@@ -38,78 +38,82 @@
 #include "arrow/flight/sql/odbc/odbcabstraction/include/odbcabstraction/logger.h"
 
 SQLRETURN SQL_API SQLAllocHandle(SQLSMALLINT type, SQLHANDLE parent, SQLHANDLE* result) {
-  return arrow::SQLAllocHandle(type, parent, result);
+  return arrow::flight::sql::odbc::SQLAllocHandle(type, parent, result);
 }
 
 SQLRETURN SQL_API SQLAllocEnv(SQLHENV* env) {
-  return arrow::SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, env);
+  return arrow::flight::sql::odbc::SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, env);
 }
 
 SQLRETURN SQL_API SQLAllocConnect(SQLHENV env, SQLHDBC* conn) {
-  return arrow::SQLAllocHandle(SQL_HANDLE_DBC, env, conn);
+  return arrow::flight::sql::odbc::SQLAllocHandle(SQL_HANDLE_DBC, env, conn);
 }
 
 SQLRETURN SQL_API SQLAllocStmt(SQLHDBC conn, SQLHSTMT* stmt) {
-  return arrow::SQLAllocHandle(SQL_HANDLE_STMT, conn, stmt);
+  return arrow::flight::sql::odbc::SQLAllocHandle(SQL_HANDLE_STMT, conn, stmt);
 }
 
 SQLRETURN SQL_API SQLFreeHandle(SQLSMALLINT type, SQLHANDLE handle) {
-  return arrow::SQLFreeHandle(type, handle);
+  return arrow::flight::sql::odbc::SQLFreeHandle(type, handle);
 }
 
 SQLRETURN SQL_API SQLFreeEnv(SQLHENV env) {
-  return arrow::SQLFreeHandle(SQL_HANDLE_ENV, env);
+  return arrow::flight::sql::odbc::SQLFreeHandle(SQL_HANDLE_ENV, env);
 }
 
 SQLRETURN SQL_API SQLFreeConnect(SQLHDBC conn) {
-  return arrow::SQLFreeHandle(SQL_HANDLE_DBC, conn);
+  return arrow::flight::sql::odbc::SQLFreeHandle(SQL_HANDLE_DBC, conn);
 }
 
 SQLRETURN SQL_API SQLFreeStmt(SQLHSTMT stmt, SQLUSMALLINT option) {
-  return arrow::SQLFreeStmt(stmt, option);
+  return arrow::flight::sql::odbc::SQLFreeStmt(stmt, option);
 }
 
 SQLRETURN SQL_API SQLGetDiagField(SQLSMALLINT handleType, SQLHANDLE handle,
                                   SQLSMALLINT recNumber, SQLSMALLINT diagIdentifier,
                                   SQLPOINTER diagInfoPtr, SQLSMALLINT bufferLength,
                                   SQLSMALLINT* stringLengthPtr) {
-  return arrow::SQLGetDiagField(handleType, handle, recNumber, diagIdentifier,
-                                diagInfoPtr, bufferLength, stringLengthPtr);
+  return arrow::flight::sql::odbc::SQLGetDiagField(handleType, handle, recNumber,
+                                                   diagIdentifier, diagInfoPtr,
+                                                   bufferLength, stringLengthPtr);
 }
 
 SQLRETURN SQL_API SQLGetDiagRec(SQLSMALLINT handleType, SQLHANDLE handle,
                                 SQLSMALLINT recNumber, SQLWCHAR* sqlState,
                                 SQLINTEGER* nativeErrorPtr, SQLWCHAR* messageText,
                                 SQLSMALLINT bufferLength, SQLSMALLINT* textLengthPtr) {
-  return arrow::SQLGetDiagRec(handleType, handle, recNumber, sqlState, nativeErrorPtr,
-                              messageText, bufferLength, textLengthPtr);
+  return arrow::flight::sql::odbc::SQLGetDiagRec(handleType, handle, recNumber, sqlState,
+                                                 nativeErrorPtr, messageText,
+                                                 bufferLength, textLengthPtr);
 }
 
 SQLRETURN SQL_API SQLGetEnvAttr(SQLHENV env, SQLINTEGER attr, SQLPOINTER valuePtr,
                                 SQLINTEGER bufferLen, SQLINTEGER* strLenPtr) {
-  return arrow::SQLGetEnvAttr(env, attr, valuePtr, bufferLen, strLenPtr);
+  return arrow::flight::sql::odbc::SQLGetEnvAttr(env, attr, valuePtr, bufferLen,
+                                                 strLenPtr);
 }
 
 SQLRETURN SQL_API SQLSetEnvAttr(SQLHENV env, SQLINTEGER attr, SQLPOINTER valuePtr,
                                 SQLINTEGER strLen) {
-  return arrow::SQLSetEnvAttr(env, attr, valuePtr, strLen);
+  return arrow::flight::sql::odbc::SQLSetEnvAttr(env, attr, valuePtr, strLen);
 }
 
 SQLRETURN SQL_API SQLGetConnectAttr(SQLHDBC conn, SQLINTEGER attribute,
                                     SQLPOINTER valuePtr, SQLINTEGER bufferLength,
                                     SQLINTEGER* stringLengthPtr) {
-  return arrow::SQLGetConnectAttr(conn, attribute, valuePtr, bufferLength,
-                                  stringLengthPtr);
+  return arrow::flight::sql::odbc::SQLGetConnectAttr(conn, attribute, valuePtr,
+                                                     bufferLength, stringLengthPtr);
 }
 
 SQLRETURN SQL_API SQLSetConnectAttr(SQLHDBC conn, SQLINTEGER attr, SQLPOINTER value,
                                     SQLINTEGER valueLen) {
-  return arrow::SQLSetConnectAttr(conn, attr, value, valueLen);
+  return arrow::flight::sql::odbc::SQLSetConnectAttr(conn, attr, value, valueLen);
 }
 
 SQLRETURN SQL_API SQLGetInfo(SQLHDBC conn, SQLUSMALLINT infoType, SQLPOINTER infoValuePtr,
                              SQLSMALLINT bufLen, SQLSMALLINT* length) {
-  return arrow::SQLGetInfo(conn, infoType, infoValuePtr, bufLen, length);
+  return arrow::flight::sql::odbc::SQLGetInfo(conn, infoType, infoValuePtr, bufLen,
+                                              length);
 }
 
 SQLRETURN SQL_API SQLDriverConnect(SQLHDBC conn, SQLHWND windowHandle,
@@ -119,7 +123,7 @@ SQLRETURN SQL_API SQLDriverConnect(SQLHDBC conn, SQLHWND windowHandle,
                                    SQLSMALLINT outConnectionStringBufferLen,
                                    SQLSMALLINT* outConnectionStringLen,
                                    SQLUSMALLINT driverCompletion) {
-  return arrow::SQLDriverConnect(
+  return arrow::flight::sql::odbc::SQLDriverConnect(
       conn, windowHandle, inConnectionString, inConnectionStringLen, outConnectionString,
       outConnectionStringBufferLen, outConnectionStringLen, driverCompletion);
 }
@@ -127,54 +131,61 @@ SQLRETURN SQL_API SQLDriverConnect(SQLHDBC conn, SQLHWND windowHandle,
 SQLRETURN SQL_API SQLConnect(SQLHDBC conn, SQLWCHAR* dsnName, SQLSMALLINT dsnNameLen,
                              SQLWCHAR* userName, SQLSMALLINT userNameLen,
                              SQLWCHAR* password, SQLSMALLINT passwordLen) {
-  return arrow::SQLConnect(conn, dsnName, dsnNameLen, userName, userNameLen, password,
-                           passwordLen);
+  return arrow::flight::sql::odbc::SQLConnect(conn, dsnName, dsnNameLen, userName,
+                                              userNameLen, password, passwordLen);
 }
 
-SQLRETURN SQL_API SQLDisconnect(SQLHDBC conn) { return arrow::SQLDisconnect(conn); }
+SQLRETURN SQL_API SQLDisconnect(SQLHDBC conn) {
+  return arrow::flight::sql::odbc::SQLDisconnect(conn);
+}
 
 SQLRETURN SQL_API SQLGetStmtAttr(SQLHSTMT stmt, SQLINTEGER attribute, SQLPOINTER valuePtr,
                                  SQLINTEGER bufferLength, SQLINTEGER* stringLengthPtr) {
-  return arrow::SQLGetStmtAttr(stmt, attribute, valuePtr, bufferLength, stringLengthPtr);
+  return arrow::flight::sql::odbc::SQLGetStmtAttr(stmt, attribute, valuePtr, bufferLength,
+                                                  stringLengthPtr);
 }
 
 SQLRETURN SQL_API SQLExecDirect(SQLHSTMT stmt, SQLWCHAR* queryText,
                                 SQLINTEGER textLength) {
-  return arrow::SQLExecDirect(stmt, queryText, textLength);
+  return arrow::flight::sql::odbc::SQLExecDirect(stmt, queryText, textLength);
 }
 
-SQLRETURN SQL_API SQLFetch(SQLHSTMT stmt) { return arrow::SQLFetch(stmt); }
+SQLRETURN SQL_API SQLFetch(SQLHSTMT stmt) {
+  return arrow::flight::sql::odbc::SQLFetch(stmt);
+}
 
 SQLRETURN SQL_API SQLExtendedFetch(SQLHSTMT stmt, SQLUSMALLINT fetchOrientation,
                                    SQLLEN fetchOffset, SQLULEN* rowCountPtr,
                                    SQLUSMALLINT* rowStatusArray) {
-  return arrow::SQLExtendedFetch(stmt, fetchOrientation, fetchOffset, rowCountPtr,
-                                 rowStatusArray);
+  return arrow::flight::sql::odbc::SQLExtendedFetch(stmt, fetchOrientation, fetchOffset,
+                                                    rowCountPtr, rowStatusArray);
 }
 
 SQLRETURN SQL_API SQLFetchScroll(SQLHSTMT stmt, SQLSMALLINT fetchOrientation,
                                  SQLLEN fetchOffset) {
-  return arrow::SQLFetchScroll(stmt, fetchOrientation, fetchOffset);
+  return arrow::flight::sql::odbc::SQLFetchScroll(stmt, fetchOrientation, fetchOffset);
 }
 
 SQLRETURN SQL_API SQLGetData(SQLHSTMT stmt, SQLUSMALLINT recordNumber, SQLSMALLINT cType,
                              SQLPOINTER dataPtr, SQLLEN bufferLength,
                              SQLLEN* indicatorPtr) {
-  return arrow::SQLGetData(stmt, recordNumber, cType, dataPtr, bufferLength,
-                           indicatorPtr);
+  return arrow::flight::sql::odbc::SQLGetData(stmt, recordNumber, cType, dataPtr,
+                                              bufferLength, indicatorPtr);
 }
 
 SQLRETURN SQL_API SQLPrepare(SQLHSTMT stmt, SQLWCHAR* queryText, SQLINTEGER textLength) {
-  return arrow::SQLPrepare(stmt, queryText, textLength);
+  return arrow::flight::sql::odbc::SQLPrepare(stmt, queryText, textLength);
 }
 
-SQLRETURN SQL_API SQLExecute(SQLHSTMT stmt) { return arrow::SQLExecute(stmt); }
+SQLRETURN SQL_API SQLExecute(SQLHSTMT stmt) {
+  return arrow::flight::sql::odbc::SQLExecute(stmt);
+}
 
 SQLRETURN SQL_API SQLBindCol(SQLHSTMT stmt, SQLUSMALLINT recordNumber, SQLSMALLINT cType,
                              SQLPOINTER dataPtr, SQLLEN bufferLength,
                              SQLLEN* indicatorPtr) {
-  return arrow::SQLBindCol(stmt, recordNumber, cType, dataPtr, bufferLength,
-                           indicatorPtr);
+  return arrow::flight::sql::odbc::SQLBindCol(stmt, recordNumber, cType, dataPtr,
+                                              bufferLength, indicatorPtr);
 }
 
 SQLRETURN SQL_API SQLCancel(SQLHSTMT stmt) {
@@ -186,16 +197,18 @@ SQLRETURN SQL_API SQLCancel(SQLHSTMT stmt) {
   });
 }
 
-SQLRETURN SQL_API SQLCloseCursor(SQLHSTMT stmt) { return arrow::SQLCloseCursor(stmt); }
+SQLRETURN SQL_API SQLCloseCursor(SQLHSTMT stmt) {
+  return arrow::flight::sql::odbc::SQLCloseCursor(stmt);
+}
 
 SQLRETURN SQL_API SQLColAttribute(SQLHSTMT stmt, SQLUSMALLINT recordNumber,
                                   SQLUSMALLINT fieldIdentifier,
                                   SQLPOINTER characterAttributePtr,
                                   SQLSMALLINT bufferLength, SQLSMALLINT* outputLength,
                                   SQLLEN* numericAttributePtr) {
-  return arrow::SQLColAttribute(stmt, recordNumber, fieldIdentifier,
-                                characterAttributePtr, bufferLength, outputLength,
-                                numericAttributePtr);
+  return arrow::flight::sql::odbc::SQLColAttribute(stmt, recordNumber, fieldIdentifier,
+                                                   characterAttributePtr, bufferLength,
+                                                   outputLength, numericAttributePtr);
 }
 
 SQLRETURN SQL_API SQLTables(SQLHSTMT stmt, SQLWCHAR* catalogName,
@@ -203,9 +216,9 @@ SQLRETURN SQL_API SQLTables(SQLHSTMT stmt, SQLWCHAR* catalogName,
                             SQLSMALLINT schemaNameLength, SQLWCHAR* tableName,
                             SQLSMALLINT tableNameLength, SQLWCHAR* tableType,
                             SQLSMALLINT tableTypeLength) {
-  return arrow::SQLTables(stmt, catalogName, catalogNameLength, schemaName,
-                          schemaNameLength, tableName, tableNameLength, tableType,
-                          tableTypeLength);
+  return arrow::flight::sql::odbc::SQLTables(stmt, catalogName, catalogNameLength,
+                                             schemaName, schemaNameLength, tableName,
+                                             tableNameLength, tableType, tableTypeLength);
 }
 
 SQLRETURN SQL_API SQLColumns(SQLHSTMT stmt, SQLWCHAR* catalogName,
@@ -213,9 +226,9 @@ SQLRETURN SQL_API SQLColumns(SQLHSTMT stmt, SQLWCHAR* catalogName,
                              SQLSMALLINT schemaNameLength, SQLWCHAR* tableName,
                              SQLSMALLINT tableNameLength, SQLWCHAR* columnName,
                              SQLSMALLINT columnNameLength) {
-  return arrow::SQLColumns(stmt, catalogName, catalogNameLength, schemaName,
-                           schemaNameLength, tableName, tableNameLength, columnName,
-                           columnNameLength);
+  return arrow::flight::sql::odbc::SQLColumns(
+      stmt, catalogName, catalogNameLength, schemaName, schemaNameLength, tableName,
+      tableNameLength, columnName, columnNameLength);
 }
 
 SQLRETURN SQL_API SQLForeignKeys(SQLHSTMT stmt, SQLWCHAR* pKCatalogName,
@@ -245,25 +258,28 @@ SQLRETURN SQL_API SQLForeignKeys(SQLHSTMT stmt, SQLWCHAR* pKCatalogName,
 }
 
 SQLRETURN SQL_API SQLGetTypeInfo(SQLHSTMT stmt, SQLSMALLINT dataType) {
-  return arrow::SQLGetTypeInfo(stmt, dataType);
+  return arrow::flight::sql::odbc::SQLGetTypeInfo(stmt, dataType);
 }
 
-SQLRETURN SQL_API SQLMoreResults(SQLHSTMT stmt) { return arrow::SQLMoreResults(stmt); }
+SQLRETURN SQL_API SQLMoreResults(SQLHSTMT stmt) {
+  return arrow::flight::sql::odbc::SQLMoreResults(stmt);
+}
 
 SQLRETURN SQL_API SQLNativeSql(SQLHDBC connectionHandle, SQLWCHAR* inStatementText,
                                SQLINTEGER inStatementTextLength,
                                SQLWCHAR* outStatementText, SQLINTEGER bufferLength,
                                SQLINTEGER* outStatementTextLength) {
-  return arrow::SQLNativeSql(connectionHandle, inStatementText, inStatementTextLength,
-                             outStatementText, bufferLength, outStatementTextLength);
+  return arrow::flight::sql::odbc::SQLNativeSql(connectionHandle, inStatementText,
+                                                inStatementTextLength, outStatementText,
+                                                bufferLength, outStatementTextLength);
 }
 
 SQLRETURN SQL_API SQLNumResultCols(SQLHSTMT stmt, SQLSMALLINT* columnCountPtr) {
-  return arrow::SQLNumResultCols(stmt, columnCountPtr);
+  return arrow::flight::sql::odbc::SQLNumResultCols(stmt, columnCountPtr);
 }
 
 SQLRETURN SQL_API SQLRowCount(SQLHSTMT stmt, SQLLEN* rowCountPtr) {
-  return arrow::SQLRowCount(stmt, rowCountPtr);
+  return arrow::flight::sql::odbc::SQLRowCount(stmt, rowCountPtr);
 }
 
 SQLRETURN SQL_API SQLPrimaryKeys(SQLHSTMT stmt, SQLWCHAR* catalogName,
@@ -285,7 +301,8 @@ SQLRETURN SQL_API SQLPrimaryKeys(SQLHSTMT stmt, SQLWCHAR* catalogName,
 
 SQLRETURN SQL_API SQLSetStmtAttr(SQLHSTMT stmt, SQLINTEGER attribute, SQLPOINTER valuePtr,
                                  SQLINTEGER stringLength) {
-  return arrow::SQLSetStmtAttr(stmt, attribute, valuePtr, stringLength);
+  return arrow::flight::sql::odbc::SQLSetStmtAttr(stmt, attribute, valuePtr,
+                                                  stringLength);
 }
 
 SQLRETURN SQL_API SQLDescribeCol(SQLHSTMT statementHandle, SQLUSMALLINT columnNumber,
@@ -293,7 +310,7 @@ SQLRETURN SQL_API SQLDescribeCol(SQLHSTMT statementHandle, SQLUSMALLINT columnNu
                                  SQLSMALLINT* nameLengthPtr, SQLSMALLINT* dataTypePtr,
                                  SQLULEN* columnSizePtr, SQLSMALLINT* decimalDigitsPtr,
                                  SQLSMALLINT* nullablePtr) {
-  return arrow::SQLDescribeCol(statementHandle, columnNumber, columnName, bufferLength,
-                               nameLengthPtr, dataTypePtr, columnSizePtr,
-                               decimalDigitsPtr, nullablePtr);
+  return arrow::flight::sql::odbc::SQLDescribeCol(
+      statementHandle, columnNumber, columnName, bufferLength, nameLengthPtr, dataTypePtr,
+      columnSizePtr, decimalDigitsPtr, nullablePtr);
 }
