e3a191f2884fa6aaa21b07c9ce9c82337d09eed5
Alina (Xi) Li <alinal@bitquilltech.com>
2025-12-17 15:31:02 -0800

    In-progress changes to enable static odbc_impl library
    pushing the changes just for saving my code. Currently odbc test still has double registration issue, work on this next. I think it should be solvable by either adding library flags or just using odbc spi shared. Because `odbc_spi_impl_test` works. And the ODBC tests worked just fine before I switched to static odbc spi impl.
    
    Fix for gtest missing issue
    
    Attempting to fix for issue:
    IMPORTANT NOTICE - DO NOT IGNORE:
    This test program did NOT call testing::InitGoogleTest() before calling RUN_ALL_TESTS().
    This is INVALID. Soon Google Test will start to enforce the valid usage.
    Please fix it ASAP, or IT WILL START TO FAIL.
    
    Issue fixed with adding `${ARROW_TEST_LINK_LIBS}`. It is needed alongside `arrow_flight_testing_shared`.

--- cpp/src/arrow/flight/sql/odbc/odbc_impl/ui/dsn_configuration_window.h
+++ cpp/src/arrow/flight/sql/odbc/odbc_impl/ui/dsn_configuration_window.h
@@ -17,7 +17,7 @@
 
 #pragma once
 
-#include "config/configuration.h"
+#include "arrow/flight/sql/odbc/odbc_impl/config/configuration.h"
 #include "ui/custom_window.h"
 
 namespace arrow::flight::sql::odbc {
