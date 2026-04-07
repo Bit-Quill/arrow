e1529675ac16f0de5056225ed122a19d493b1037
Alina (Xi) Li <alinal@bitquilltech.com>
2026-04-07 15:49:50 -0700

    Clean up PR

--- ci/scripts/cpp_test.sh
+++ ci/scripts/cpp_test.sh
@@ -55,7 +55,7 @@ if ! type minio >/dev/null 2>&1; then
 fi
 case "$(uname)" in
   Linux)
-    # exclude_tests+=("arrow-flight-sql-odbc-test") # -AL- temp disable!
+    exclude_tests+=("arrow-flight-sql-odbc-test")
     n_jobs=$(nproc)
     ;;
   Darwin)
